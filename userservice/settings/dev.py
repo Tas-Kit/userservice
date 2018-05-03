a7b4a07f7b66dffee25e1d32212951c81f1320fa
Lazy-Y <zhenyanz@usc.edu>
2018-05-02 20:48:25 -0700

    update settings

---
+++ userservice/settings/dev.py
@@ -0,0 +1,7 @@
+from userservice.settings.basic import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'b9rcz^eu7sx%rs3%e=wu_s!gs4)a7p42!=kuj)bbfva1v0d^ju'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
